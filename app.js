bc1551bf07631b3c578770787a9a5554b96565cd
Austin Jones <makomaster2@gmail.com>
2022-02-23 13:45:32 -0600

    finished project and added styling

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ $(function () {
 
     var FRIEND_NAME = ['Branwin', 'Cody', 'Bailey', 'Hunter', 'Reed'];
     var LOCATION_NAME = ['bathroom', 'pantry', 'pool', 'closet', 'attic', 'bedroom', 'car', 'office', 'garage', 'backyard'];
-    var WEAPON_NAME = ['banana', 'Squid Games', 'AVADAKEDAVRA!', 'CRUCIO!', 'vape', 'shoe', 'wire hanger', 'emotional damage', 'guilt', 'Sharingan', 'Death Star', 'chicken sticks', 'katana', 'package', 'bugs', 'CORONAVIIIRUS', 'Tic Tac', 'Reeses Pieces', 'Hobbit', 'DnD dice']
+    var WEAPON_NAME = ['banana', 'Squid Games', 'AVADAKEDAVRA!', 'CRUCIO!', 'vape', 'shoe', 'wire hanger', 'emotional damage', 'guilt', 'Sharingan', 'Death Star', 'chicken sticks', 'spaghetti noodle', 'finger', 'bugs', 'CORONAVIIIRUS', 'Tic Tac', 'Reeses Piece', 'prosthetic leg', 'KAMEHAMEHA!']
+    var container = $('<div>');
+    $('body').append(container);
 
     for (i = 1; i <= 100; i++) {
         var h3 = $(`<h3>Accusation ${i}</h3>`);
-        $('body').append(h3);
+        $(container).append(h3)
 
         var propertyObject = {
             num: i,
@@ -15,8 +17,13 @@ $(function () {
             location: LOCATION_NAME[Math.floor(Math.random() * LOCATION_NAME.length)]
         }
 
-        $(h3).on('click', function () {
-            alert(`Accusation ${propertyObject.num}: I accuse ${propertyObject.name}, with the ${propertyObject.weapon} in the ${propertyObject.location}!`)
+        $(h3).on('click', alertFunction.bind(propertyObject))
+        $(h3).on('click', function() {
+            $(this).css('color', 'grey');
         })
+
+        function alertFunction() {
+            alert(`Accusation ${this.num}: I accuse ${this.name}, with the ${this.weapon} in the ${this.location}!`)
+        }
     }
 })
